bc2615314a69859fcbe44e835a811df35a07b4d0
laharin05 <82572625+laharin05@users.noreply.github.com>
2021-04-16 23:13:26 +0100

    1st commit
    Python Datatypes, Variables

---
+++ task.py
@@ -0,0 +1,87 @@
+print("hello World")
+if 5 > 2:
+    print("Five is greater than two!")
+
+#This is a comment
+print("Hello World")
+"""
+
+This is a comment
+written in 
+more that just one line
+"""
+
+x = 50
+print(x)
+
+
+x=5; y=10;
+print(x+y)
+
+x = 5; y = 10; z= x + y;
+print(z)
+
+x = 5
+print(type(x))
+
+x = "Hello World"
+print(type(x))
+
+x = 20.5
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+x = True
+print(type(x))
+
+x = 5.5
+x = float(x)
+print(x)
+
+x = 5.5
+x = int(x)
+print(x)
+
+x = 5
+x = complex(x)
+print(x)
+
+x = "Hello World"
+print(len(x))
+
+txt = "Hello World"
+x = txt[0]
+print(x)
+
+txt = "Hello World"
+x = txt[2:5]
+print(x)
+
+txt = " Hello World "
+x = txt.strip()
+print(x)
+
+txt = "Hello World"
+txt = txt.upper()
+print(txt)
+
+txt = "Hello World"
+txt = txt.lower()
+print(txt)
+
+txt = "Hello World"
+txt = txt.replace("H", "J")
+print(txt)
+
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
